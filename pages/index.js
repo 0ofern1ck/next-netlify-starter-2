f2bd684988efdba0a2810f767865a3fb466ec657
✧Enderman✧ <79604764+0ofern1ck@users.noreply.github.com>
2022-11-12 12:54:35 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Rune Games V.1!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is a version of this new website im working on with games! <code>pages/index.js</code>
         </p>
       </main>
 
